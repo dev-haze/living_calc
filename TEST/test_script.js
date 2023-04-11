0ee08129f1c0e8a3816f3328ff2c4cb75babb203
dev-haze <62634831+dev-haze@users.noreply.github.com>
2023-04-11 19:27:44 +0900

    re

---
+++ TEST/test_script.js
@@ -0,0 +1,17 @@
+class Test_c{
+    constructor(){
+        var str="";
+        var dat="";
+    }
+    
+    input(str){
+        this.str=str;
+    }
+    print(){
+        alert(this.str);
+    }
+    print_direct(str){
+        alert(str);
+    }
+
+}
